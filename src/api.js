0f76c089ce153f7ab634103bfcb4614952ea4b96
Himanshusaini12 <107533833+Himanshusaini12@users.noreply.github.com>
2023-03-19 14:50:06 +0000

    done

---
+++ src/api.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import song from './song.mp3';
+import './gita.css'
+function GitaQuery() {
+  const [query, setQuery] = useState('');
+  const [response, setResponse] = useState('');
+  const audio = new Audio(song);
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setResponse('');
+    
+    audio.play();
+    const url = `https://nod.himasaini6.repl.co/gita?q=${encodeURIComponent(query)}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    setResponse(data.response);
+    console.log(data.response);
+
+
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label className='label'>
+          Enter your query:
+          <input className='count-input ' type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
+        </label>
+        <button className='submit-button' type="submit">Submit</button>
+      </form>
+      {response && (
+        <div>
+          <h2>Solution:</h2>
+          <p className='res'>{response}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default GitaQuery;
